5f7cd94ec74663dc88fcf1a36ec2e03f392566f2
tao.wang <tao.wang@xyroyale.com>
2019-12-10 16:28:45 +0800

    投放系统>>>预算补充修改

--- src/store/modules/overseas-configuration/budget/index.js
+++ src/store/modules/overseas-configuration/budget/index.js
@@ -1,106 +1,107 @@
 import http from 'src/services/http'
-var _1day = 1000 * 3600 * 24
+import {
+    Number
+} from 'core-js';
+
+function getObjectByTimestamp() {
+    return {
+        timestamp: new Date().getTime()
+    }
+}
+
 export default {
-  namespaced: true,
-  state: {
-    date: moment(new Date().getTime() - _1day).format("YYYY-MM"),
-    data: null,
-    config: {}
-  },
-  mutations: {
-    set(state, { key, val }) {
-      state[key] = val
-    }
-  },
-  getters: {
-    getData(state) {
-      if (state.data && state.data.length) {
-        var first = state.data[0]
-        var { config } = state
-        if (!config.keys) {
-          config.keys = Object.keys(first)
-          console.log(config.keys)
-        }
-        if (!config.index) {
-          config.index = {
-            dateIndex: 0,
-            appIdIndex: 1,
-            gameIndex: 2,
-            budgetIndex: 3
-          }
-        }
-        if (!config.tableKey) {
-          config.tableKey = [{
-            key: config.keys[config.index.dateIndex],
-            sortable: false
-          }, {
-            key: config.keys[config.index.appIdIndex],
-            sortable: false
-          }, {
-            key: config.keys[config.index.gameIndex],
-            sortable: false
-          }, {
-            key: config.keys[config.index.budgetIndex],
-            sortable: false
-          }]
-        }
-        return state.data
-      } else {
-        return null
-      }
-    }
-  },
-  actions: {
-    getData(context, param) {
-
-      var {
-        commit,
-        state,
-        dispatch,
-        getters,
-        rootGetters,
-        rootState,
-      } = context
-
-      var url = "/query/" + 'fn_oas_budget_costs'
-      console.log(param)
-      return new Promise(resolve => {
-        http.post(url, param).then(data => {
-          if (data.code === 401) {
-            commit('set', { key: 'data', val: data.state[0] })
-            resolve(data)
-          }
-        })
-      })
-
+    namespaced: true,
+    state: {
+        //time
+        date:null,
+        mailUrl: null,
+        // 分成比例
+        proportion:0.00,
+        //游戏列表
+        gameList: [],
+        //分成补充数据
+        dividedInto:[],
+        //游戏列表下标
+        gameListIndex:null,
     },
+    mutations: {
+        setDate(state, data) {
+            state.date = data
+        },
+        setProportion(state,data){
+            state.proportion = data
+        },
+        setGameListIndex(state,data){
+            state.gameListIndex = data
+        },
+        set_gameList(state, data) {
+            state.gameList = data
+        },
+        set_dividedInto(state, data) {
+            state.dividedInto = data
+        },
+    },
+    getters: {
+        getGameList(state, getters){
+            var gameList = [];    //返回的所有数据
+            if (state.gameList && state.gameList[0]) { 
+                gameList = state.gameList[0]
+            }
+            console.log('gamelist--------------->>>>>', gameList)
+            return gameList
+        },
+        getTableData(state,getters){
+            var arr = []
+            if (state.dividedInto && state.dividedInto[0]) { 
+                arr = state.dividedInto[0]
+            }
+            return arr
+        }
+    },
+    actions: {
+        getReportInfo(context, {
+            params,
+            tag,
+        }) {
+            var {
+                commit,
+                state,
+                dispatch,
+                getters,
+                rootGetters,
+                rootState,
+            } = context
+            var url = '/query/fn_oas_budget_costs'
+            console.log("Params:->", params)
 
-    updateData(context, param) {
+            var promise1 = new Promise((resolve) => {
+                http.post(url, params).then(data => {
+                    if (data.code == 401) {
+                        commit("set_" + tag, data.state);
+                        resolve(data.state)
+                    } else {
+                        this.$notify.warning({
+                            message: data.message
+                        })
+                        consoel.error(message)
+                    }
+                })
+            })
+            if (tag.indexOf('zone') !== -1 || tag === 'system') {
+                var promise2 = new Promise(resolve => {
+                    this.dispatch('overseas_common_realTime/getRegionMap').then(() => {
+                        resolve()
+                    })
+                })
+                return Promise.all([
+                    promise1,
+                    promise2
+                ])
+            } else {
+                return promise1
+            }
 
-      var {
-        commit,
-        state,
-        dispatch,
-        getters,
-        rootGetters,
-        rootState,
-      } = context
-      var url = "/import/" + 'fn_oas_budget_costs'
-      console.log(param)
-      return new Promise(resolve => {
-        http.post(url, param).then(data => {
-          console.log(data)
-          if (data.code === 401) {
-            console.log(state.code)
-            commit('set', { key: 'data', val: data.state[0] })
-            console.log(state)
-            getters.getData
-          }
-        })
-      })
 
+        }
     }
-
-
-  }
 }
--- src/views/modules/data/budget/index.vue
+++ src/views/modules/data/budget/index.vue
@@ -1,152 +1,227 @@
 <template>
-  <div class="data-budget">
-    <div class="first-wrap">
-      <my-row>
-        <div class="time-picker">
-          <el-button-group class="group">
-            <el-button size="medium">
-              <span>日期</span>
-            </el-button>
-            <el-date-picker size="medium" @change="dateChange" v-model="pickerOptions.date" type="month"  placeholder="选择月"> </el-date-picker>
-            <!-- <el-date-picker size="medium" @change="dateChange" v-model="pickerOptions.date" type="date" placeholder="选择日期"> </el-date-picker> -->
-          </el-button-group>
-        </div>
-
-        <el-button size="medium" @click="excel()">
-          <span>下载模板</span>
-        </el-button>
-
-        <el-button size="medium" @click="upload()">
-          <span>上传预算</span>
-          <form v-show="false" id="my-form" :action="baseUrl + '/import/fn_oas_budget_costs'" method="post" enctype="multipart/form-data">
-            <input type="file" name="file" ref="upload">
-          </form>
-        </el-button>
-
-        <el-button size="medium" @click="query()">
-          <span>查询</span>
-        </el-button>
-
-      </my-row>
-    </div>
-
-    <div class="second-wrap">
-      <my-row v-if="$$data">
-        <el-table :data="$$data">
-          <el-table-column v-for="(item, i) in _state.config.tableKey" :key="i" :prop="item.key" :label="item.key" :sortable="item.sortable"></el-table-column>
+  <div>
+    <!-- 分成补充查询条件 -->
+    <my-row class="selection-box">
+      <div class="banner">
+        <!-- 日期 -->
+        <section style="margin-left:20px">
+          <el-date-picker size="medium" v-model="date"  type="month"  placeholder="选择月"> </el-date-picker>
+        </section>
+        <!-- 添加 -->
+        <el-button size="medium" class="search" @click="addData()" style="margin-left:20px">查询</el-button>
+      </div>
+    </my-row>
+    <!-- 表格 -->
+      <div v-show="$store.state.o_r_delivery.tableIsVisible" class="table-item">
+        <el-table
+          class="tb-edit"
+          border
+          :header-cell-style="{background:'#f2f2f2'}"
+          :data="$store.getters['o_c_budget/getTableData'].filter(data => !search || data.app_name.toLowerCase().includes(search.toLowerCase()) || (data.app_id+'').includes(search))"
+          @cell-dblclick='handleCellClick'
+          :row-class-name="tableRowClassName"
+        >
+          <el-table-column
+            v-for="(item, i) in (Object.keys($store.getters['o_c_budget/getTableData'].length?$store.getters['o_c_budget/getTableData'][0]:[]))"
+            :key="i"
+            :sortable="i>3"
+            :prop="item"
+            :label="item"
+            :width="i==1 ? 280:''"
+            v-if="(i<=2)"
+          >
+          </el-table-column>
+          <el-table-column
+            v-for="(item, i) in (Object.keys($store.getters['o_c_budget/getTableData'].length?$store.getters['o_c_budget/getTableData'][0]:[]))"
+            :key="i"
+            :sortable="i==3"
+            :prop="item"
+            :label="item"
+            :width="i==3 ? 280:''"
+            v-if="i==3"
+          >
+          <template slot-scope="scope">
+                <el-input v-show="activityIndex == scope.$index" size="small" autofocus type="text" placeholder="请输入预算"
+                v-model="$store.getters['o_c_budget/getTableData'][scope.$index][item]" 
+                @blur="blurInput(scope.$index)"
+                ></el-input> 
+                <span v-show="activityIndex != scope.$index">{{$store.getters['o_c_budget/getTableData'][scope.$index][item]}}</span>
+            </template>
+          </el-table-column>
+          <el-table-column align="right" width="230">
+            <template slot="header" slot-scope="scope">
+              <el-input
+                v-model="search"
+                size="mini"
+                placeholder="输入关键字搜索"/>
+            </template>
+            <template slot-scope="scope">
+                <el-button
+                @click.native.prevent="changeRow(scope.$index,)"
+                type="primary"
+                size="small">
+                修改
+                </el-button>
+            </template>
+          </el-table-column>
+           <!-- <el-table-column
+            v-if="$store.getters['o_c_budget/getTableData'].length!=0"
+            label="操作"
+            width="160">
+            <template slot-scope="scope">
+                <el-button
+                @click.native.prevent="changeRow(scope.$index,)"
+                type="primary"
+                size="small">
+                修改
+                </el-button>
+            </template> -->
+            <!-- </el-table-column> -->
         </el-table>
-      </my-row>
-    </div>
+      </div>
   </div>
 </template>
-
 <script>
-import Loader from 'src/views/system/loader/loader.js'
-import Http, { baseUrl } from 'src/services/http'
 export default {
-  name: "data-budget",
-  components: {},
   data() {
     return {
-      SMN: "o_c_budget",
-      pickerOptions: {
-        date: null,
-      },
-      baseUrl,
-      promise1: null
-    }
-  },
-  computed: {
-    _state() {
-      return this.$store.state[this.SMN]
-    },
-    $$data() {
-      return this.$store.getters[this.SMN + '/getData']
-    }
-  },
-  watch: {},
-  methods: {
-    upload() {
-      this.$refs.upload.click()
-    },
-    excel() {
-      window.open('http://121.10.140.56:8115/tpl/海外投放预算模板.xlsx')
-    },
-    dateChange(date) {
-      this.$store.commit(this.SMN + '/set', {
-        key: 'date',
-        val: moment(date).format("YYYY-MM")
-      })
-    },
-    query() {
-      this.$store.dispatch(this.SMN + '/getData', { count_date: this._state.date+'-01' }).then(data => {
+      date: '',         //日期
+      delIndex:0,      //del下标
+      activityIndex:-1,
+      search:'',
 
-      })
-    },
-    fileChange() {
-      this.promise1.then(() => {
-        Loader.load()
-        $('#my-form').ajaxSubmit({
-          dataType: 'json',
-          method: 'post',
-          xhrFields: {
-            withCredentials: true
-          },
-          success: (data) => {
-            console.log(data)
-            if (data.code === 401) {
-              this.$notify({
-                type: "success",
-                message: "上传成功"
-              });
-              Loader.loadend()
-            } else {
-              this.$notify({
-                type: "error",
-                message: data.state
-              });
-              Loader.loadend()
-            }
-          },
-          error: (data) => {
-            this.$notify({
-              type: "error",
-              message: '操作失败'
-            });
-            Loader.loadend()
-          }
-        })
-      })
     }
+  },
+  computed:{
+    $$getTableData(){
+      return this.$store.getters["o_c_budget/getTableData"];
+    },
+    _state() {
+      return this.$store.state.o_c_budget;
+    },
   },
   created() {
-    this.pickerOptions.date = new Date(this._state.date)
-    this.query()
+    this.dataInit();
+    // 获取渠道列表,
+    if (!this._state.gameList.length) this.getData('dividedInto',1);
   },
-  mounted() {
-    this.$refs.upload.addEventListener("change", this.fileChange)
-    this.promise1 = new Promise(resolve => {
-      require.ensure([], () => {
-        require("jquery-form")
-        resolve()
-      })
-    })
+  watch: {
+    
+      date(newValue,oldValue){
+           console.log(newValue,moment(newValue).add().format("YYYY-MM-DD"));
+
+          this.$store.commit("o_c_budget/setDate",  moment(newValue).add().format("YYYY-MM-DD"));
+      },
   },
-  beforeDestroy() {
-    this.$refs.upload.removeEventListener("change", this.fileChange)
+  methods:{
+      //分成比例修改   
+      blurInput(index){
+        this.activityIndex = -1
+      },
+      //添加行的索引值   
+      tableRowClassName({row, rowIndex}) {
+        row.index = rowIndex;
+        return '';
+      },
+     // 双击修改分成比例  
+      handleCellClick(row, column, cell, event){
+          var {label} = column;
+          console.log('row[column.property]',String(row[column.property] ))
+          if (label == 'cost') {
+              this.activityIndex = row.index
+          }
+      },
+    // 修改数据
+    changeRow(index){
+      // 预算是否为空
+        if (!this.$store.getters['o_c_budget/getTableData'][index]['cost']) {
+           return Utils.Notification.warning({message: '请将预算填写完整'})
+        }
+        if (confirm("确认修改？")) {
+          var params = {
+            in_count_date: this._state.dividedInto[0][index]['统计时间'],  //开始日期
+            in_app_id: this._state.dividedInto[0][index].app_id,                     //游戏id
+            in_app_name:this._state.dividedInto[0][index].app_name,             //游戏名称
+            in_buget_cost:this._state.dividedInto[0][index].cost.format(2),               //预算(保留两位小数)
+            in_operator_type:2,                //查询类型(1:预算信息,2:修改预算)
+          };
+          this.$store.dispatch("o_c_budget/getReportInfo", { params, tag: 'dividedInto' })
+        }
+    },
+    //添加数据
+    addData(){
+         if (!this.date) {
+          Utils.Notification.warning({
+              message: '请选择查询日期'
+          })
+          return
+        }
+        this.getData('dividedInto',1)
+    },
+    // 更新数据
+    getData(tag,index) {
+        // 获取渠道
+        var params = {
+          in_count_date: this.date?moment(this.date).add().format("YYYY-MM-DD"):moment().startOf('month'),       //开始日期
+          in_app_id: 0,                     //游戏id
+          in_app_name:'',             //游戏名称
+          in_buget_cost:0.00,               //预算(保留两位小数)
+          in_operator_type:index,                //查询类型(1:预算信息,2:修改预算)
+        };
+      this.$store.dispatch("o_c_budget/getReportInfo", { params, tag: tag })
+    },
+    // 初始化
+    dataInit() {
+      // 日期初始化
+      if (this._state.date) {
+        this.date = this._state.date;
+      } else {
+        this.date = moment().startOf('month')
+      }
+    },
   }
 }
 </script>
+<style lang="scss" scoped>
 
-<style lang="scss">
-.components.data-budget {
-  .first-wrap {
-    float: left;
-    margin: 15px;
-  }
-  .group {
+input[type=number] {
+    -moz-appearance:textfield;
+}
+input[type=number]::-webkit-inner-spin-button,
+input[type=number]::-webkit-outer-spin-button {
+    -webkit-appearance: none;
+    margin: 0;
+}
+
+.banner {
+  width: 100%;
+  display: flex;
+  justify-content: start;
+  // border: 1px solid;
+  .mail{
     display: flex;
-    margin-right: 15px;
+    justify-content: flex-end;
+    flex-grow: 1;
+    margin: 0px 10px 0 0;
+  }
+  .number{
+    background-color: #FFF;
+    background-image: none;
+    border-radius: 4px;
+    border: 1px solid #DCDFE6;
+    color: #606266;
+    height: 40px;
+    line-height: 40px;
+    padding: 0 15px;
+    width: 100px;
   }
 }
+
+.table-item{
+  border: 15px solid #e7e1ea;
+  width: 1200px;
+  height: auto;
+  margin: 20px;
+}
 </style>
+
