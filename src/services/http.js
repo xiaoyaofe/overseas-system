d7ca0b72e8df400cb1383ffa1a2f692c6ca2f05b
tao.wang <tao.wang@xyroyale.com>
2019-12-18 11:27:33 +0800

    20181218

--- src/services/http.js
+++ src/services/http.js
@@ -12,12 +12,12 @@ let port = 8111
 
 // requrl = "http://172.16.3.171"
 // port = 7011
-
 // var baseUrl = requrl + ':' + port + '/api/v1.0'
 
 // 外网
 requrl = "https://integerOas.pocketgamesol.com"
 port = ''
+
 var baseUrl = requrl + '/api/v1.0'
 export {
   baseUrl
