c31ff57bb234550b069ca9e05b8883f18065c16f
tao.wang <tao.wang@xyroyale.com>
2020-11-20 17:20:22 +0800

    修改请求地址

--- src/services/http.js
+++ src/services/http.js
@@ -16,18 +16,19 @@ var baseUrl = requrl + ':' + port + '/api/v2.0'
 
 
 // 外网
-// requrl = "http://113.107.160.59:7011/"
+// requrl = "https://integerOas.pocketgamesol.com"
 // port = ''
 // var baseUrl = requrl + '/api/v2.0'
+
 
 export {
   baseUrl
 }
 export default {
   // 内网
-  // host: requrl + ':' + port,
+  host: requrl + ':' + port,
   // 外网
-  host: requrl,
+  // host: requrl,
   jsonp(url, data, close) {
     let ajax = $.ajax({
       url: url ? baseUrl + url : baseUrl + data.url,
